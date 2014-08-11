eb8e5841f2b6f26afea55eaa22ce1e2289bc6222
Pasha Sadikov <psadikov@bitsighttech.com>
2014-08-11 17:52:55 -0400

    Updating regex to support dashes

--- fabric_bolt/roles/urls.py
+++ fabric_bolt/roles/urls.py
@@ -7,7 +7,7 @@ urlpatterns = patterns(
     '',
     url(r'^$', views.RoleList.as_view(), name='roles_role_list'),
     url(r'^create$', views.RoleCreate.as_view(), name='roles_role_create'),
-    url(r'^update/(?P<pk>[^\s-]+)/', views.RoleUpdate.as_view(), name='roles_role_update'),
-    url(r'^view/(?P<pk>[^\s-]+)/', views.RoleDetail.as_view(), name='roles_role_detail'),
-    url(r'^delete/(?P<pk>[^\s-]+)/', views.RoleDelete.as_view(), name='roles_role_delete'),
+    url(r'^update/(?P<pk>[^\s]+)/', views.RoleUpdate.as_view(), name='roles_role_update'),
+    url(r'^view/(?P<pk>[^\s]+)/', views.RoleDetail.as_view(), name='roles_role_detail'),
+    url(r'^delete/(?P<pk>[^\s]+)/', views.RoleDelete.as_view(), name='roles_role_delete'),
 )
